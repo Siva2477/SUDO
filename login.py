995d6dc9b5347662030297dd11e4c9d86b813575
Shiva2477 <katamsivaiah2477@gmail.com>
2024-09-24 11:57:29 +0000

    loginpage

---
+++ login.py
@@ -0,0 +1,21 @@
+Hii
+import os
+from datetime import datetime
+from flask import Flask, render_template, make_response
+
+app = Flask(__name__)
+
+
+def get_app_debug_info():
+    cfg_items = {k: v for k, v in os.environ.items()}
+    cfg_items['datetime'] = datetime.now().isoformat()
+    return cfg_items
+
+
+@app.route('/')
+def welcome():
+    return {
+        'msg': 'Hello World! This is a simple Python app using Flask! But wait there is more!',
+        'endpoints': ['/', '/ping', '/debug', '/debug/ui']
+    }
+
